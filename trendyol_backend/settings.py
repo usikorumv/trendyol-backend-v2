e6615c05c86b460c0b3c499ea324a60bbc4b0487
Usik <59968956+usikorumv@users.noreply.github.com>
2022-09-07 15:52:44 +0600

    Update settings.py

--- trendyol_backend/settings.py
+++ trendyol_backend/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+ # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-9o#g5v4#m2(*5#*!8nz0m&s3rt6(ng+hbd&)z-t&3ak+n9i-10'
 
 # SECURITY WARNING: don't run with debug turned on in production!
